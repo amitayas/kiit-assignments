3a096b33a224e32a02dad3af92c77cf077773bf7
ami-ba <amitayasb@gmail.com>
2021-11-21 12:28:45 +0530

    added 7th assignment

--- kiit6/mult_table.c
+++ kiit6/mult_table.c
@@ -21,7 +21,7 @@ int main ()
     
     for (i = 1 ; i <= n ; i++) {
         for (j = 1; j <=10 ; j++) {
-            printf("%d * %d = %d\n", i , j , (i*j));
+            printf("%d x %d = %d\n", i , j , (i*j));
         }
         printf("\n");
     }
